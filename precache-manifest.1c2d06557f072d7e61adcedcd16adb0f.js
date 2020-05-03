053dac08e51a4f6467c1339c1c225a39494588bd
hyperbora <hyperbora@gmail.com>
2020-05-03 10:56:46 +0900

    Updates

---
+++ precache-manifest.1c2d06557f072d7e61adcedcd16adb0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c80e9c02690ccfe97ffb0520fe0563f",
+    "url": "/react-web-app/index.html"
+  },
+  {
+    "revision": "9898550e77d9cd1bbd96",
+    "url": "/react-web-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "70f353a8b58571266819",
+    "url": "/react-web-app/static/js/2.9cd71a41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-web-app/static/js/2.9cd71a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9898550e77d9cd1bbd96",
+    "url": "/react-web-app/static/js/main.0d4f2a96.chunk.js"
+  },
+  {
+    "revision": "18e0d397e0b35e9a5c21",
+    "url": "/react-web-app/static/js/runtime-main.d52a2040.js"
+  }
+]);
